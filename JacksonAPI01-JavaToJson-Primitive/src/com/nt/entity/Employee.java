f471ac59d3831741e476b98f87d3748ec9de118c
hp <hp@venkat>
2021-07-02 10:45:08 +0530

    Java Object to JSON

--- JacksonAPI01-JavaToJson-Primitive/src/com/nt/entity/Employee.java
+++ JacksonAPI01-JavaToJson-Primitive/src/com/nt/entity/Employee.java
@@ -1,6 +1,6 @@
 package com.nt.entity;
 
-import org.codehaus.jackson.annotate.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- JacksonAPI01-JavaToJson-Primitive/src/com/nt/test/Test.java
+++ JacksonAPI01-JavaToJson-Primitive/src/com/nt/test/Test.java
@@ -1,26 +1,23 @@
 package com.nt.test;
 
-
-import org.codehaus.jackson.map.ObjectMapper;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nt.entity.Employee;
 
 public class Test {
 
 	public static void main(String[] args) {
-		
-		Employee emp=new Employee(846736, "Venkateswarlu", 30670.0f, "Verkst@123");
-		
+
+		Employee emp = new Employee(846736, "Venkateswarlu", 30670.0f, "Verkst@123");
+
 		try {
-			ObjectMapper om=new ObjectMapper();
-			
-			String json=om.writeValueAsString(emp);
+			ObjectMapper om = new ObjectMapper();
+
+			String json = om.writeValueAsString(emp);
 			System.out.println(json);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 
 }
